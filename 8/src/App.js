9c724bfcca9cadca2aa45d1dbf9346cd7d3cd8dc
silloi <silloi1993@gmail.com>
2021-12-17 23:32:01 +0900

    replace

--- 8/src/App.js
+++ 8/src/App.js
@@ -1,35 +1,13 @@
-import React from "react";
-import { FixedSizeList } from "react-window";
-import faker from "faker";
-
-const bigList = [...Array(5000)].map(() => ({
-  name: faker.name.findName(),
-  email: faker.internet.email(),
-  avatar: faker.internet.avatar()
-}));
+import React, { useState } from "react";
+import SearchForm from "./SearchForm";
+import GitHubUser from "./GitHubUser";
 
 export default function App() {
-  const renderRow = ({ index, style }) => (
-    <div style={{ ...style, ...{ display: "flex" } }}>
-      <img
-        src={bigList[index].avatar}
-        alt={bigList[index].name}
-        width={50}
-      />
-      <p>
-        {bigList[index].name} - {bigList[index].email}
-      </p>
-    </div>
-  );
-
+  const [login, setLogin] = useState("moonhighway");
   return (
-    <FixedSizeList
-      height={window.innerHeight}
-      width={window.innerWidth - 20}
-      itemCount={bigList.length}
-      itemSize={50}
-    >
-      {renderRow}
-    </FixedSizeList>
+    <>
+      <SearchForm value={login} onSearch={setLogin} />
+      <GitHubUser login={login} />
+    </>
   );
 }
