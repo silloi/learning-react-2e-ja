ebd83d2369509d877f6a718c5801c62eb48bbbf5
silloi <silloi1993@gmail.com>
2021-12-19 11:52:19 +0900

    feature: add eslint

---
+++ 10/src/sample.js
@@ -0,0 +1,29 @@
+// import React, { useState } from "react";
+
+// const gnar = "gnarly";
+
+// const info = ({
+//   file = __filename,
+//   dir = __dirname,
+// }) => (
+//   <p>
+//     {dir}: {file}
+//   </p>
+// );
+
+// switch (gnar) {
+//   default:
+//     console.log("gnarly");
+//     break;
+// }
+
+// function gnar() {
+//   const [nickname, setNickname] = useState(
+//     "dude"
+//   );
+//   return <h1>gnarly</h1>;
+// }
+
+function Image() {
+  return <img src="/img.png" />;
+}
